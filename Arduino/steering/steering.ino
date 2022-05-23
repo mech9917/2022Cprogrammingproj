3803984a6d72e0b8f46877624a243ee00e23b704
Heesung Kim <mech9917@gmail.com>
2022-05-23 11:25:43 +0900

    ino in project file
    05231125
    move path of ino file

--- Arduino/steering/steering.ino
+++ Arduino/steering/steering.ino
@@ -3,7 +3,8 @@
  * Senior in Department of Mechanical Engineering,
  * Sogang University, Republic of Korea
  */ 
-/* Servo.h library is compatible with the 
+/* 
+ *  Servo.h library is compatible with the 
  * avr, megaavr, sam, samd, nrf52, stm32f4, mbed, 
  * mbed_nano, mbed_portenta, mbed_rp2040 
  * architectures so you should be able to use it at adequate boards.
@@ -15,9 +16,9 @@ Servo front;
 
 char state;             // char type variable received from Visual Studio
 const int servoPin = 9; // servo pin attacted to pin 9
-const int angle_i=80;   // initialization of servo angle (range:55~105)
-int value0=angle_i;
-int value=value0;
+const int angle_i = 80;   // initialization of servo angle (range:55~105)
+int value0 = angle_i;
+int value = value0;
 
 void setup(){
   // put your setup code here, to run once:
@@ -31,24 +32,24 @@ void setup(){
 void loop(){
   // put your main code here, to run repeatedly:
   if(Serial.available()){
-    state=Serial.read(); // receive char value from Serial (in VS)
-    if (state == 'L'){value=55;}
-    else if (state =='l'){value=66;}
-    else if (state =='N'){value=80;}
-    else if (state =='r'){value=94;}
-    else if (state=='R'){value=105;}
+    state = Serial.read(); // receive char value from Serial (in VS)
+    if (state == 'L'){ value = 55; }
+    else if (state == 'l'){ value = 66; }
+    else if (state == 'N'){ value = 80; }
+    else if (state == 'r'){ value = 94; }
+    else if (state == 'R'){ value = 105; }
     if (value0 < value)
     {
-      for (int i = value0 ; i < value; i++)
+      for (int i = value0; i < value; i++)
       {
         front.write(i);
         delay(15);
       }
-    value0=value;
+    value0 = value;
     }
     else
     {
-      for (int i = value0; i > value ; i--)
+      for (int i = value0; i > value; i--)
       {
         front.write(i);
         delay(15);
